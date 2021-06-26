18da878236732f663a94f7bc30d12b8fefb02620
Rohit Roy <64767006+blackbriar07@users.noreply.github.com>
2021-06-26 23:23:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('osdF2N9u1mUdK+oiIE6ejdR0uMOAAWqt+AFovNeRLeUqp3j7Nee0PN7T9qelXzRv7sEr7wBrblfKeosNFNd9eqbgIueG1znQpmZ+4IBp6YooJqQZI/boaG3cOZdCPoF7rPQEjLiY4tACh4ekdZ9EewdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('4wiAT/cN++uvxW2INvKlHBAdblgn9kjJlYqgi53RFOiv05riKDHv9cVSEuIM12CR7sEr7wBrblfKeosNFNd9eqbgIueG1znQpmZ+4IBp6YqT3XVC8tUNsqMiwIXcEL/2qXLZEp4+UWjQzfbx8WcbkgdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('e23a85130af721661255f935d54705ed')
 
@@ -31,6 +31,19 @@ def callback():
     return 'OK'
 
 # 處理訊息
+
+@handler.add(MessageEvent, message = TextMessage)
+def handle_text_message(event):
+    text = event.message.text
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+            line_bot_api.reply_message(event.reply_token, [ TextSendMessage(text='Display name: ' +profile.display_name),TextSendMessage(text='Status message: ' +profile.status_message)])
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Bot cant use profile API without user ID"))
+    elif text == 'bye':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "leaving"))
+'''
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg_from_user = (event.message.text).lower()
@@ -40,7 +53,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="thats awesome"))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="I did not understand "))
-
+'''
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
