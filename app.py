c6893ded3396b190b227ff00e198d67e6c037dd0
Rohit Roy <64767006+blackbriar07@users.noreply.github.com>
2021-06-27 23:44:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,54 @@ def handle_text_message(event):
                                 ]
                             )
                         )
+   
+    message_carousel = TemplateSendMessage(
+                            alt_text='Carousel template',
+                            template=CarouselTemplate(
+                                columns=[
+                                    CarouselColumn(
+                                        thumbnail_image_url='https://example.com/item1.jpg',
+                                        title='this is menu1',
+                                        text='description1',
+                                        actions=[
+                                            PostbackTemplateAction(
+                                                label='postback1',
+                                                text='postback text1',
+                                                data='action=buy&itemid=1'
+                                            ),
+                                            MessageTemplateAction(
+                                                label='message1',
+                                                text='message text1'
+                                            ),
+                                            URITemplateAction(
+                                                label='uri1',
+                                                uri='http://example.com/1'
+                                            )
+                                        ]
+                                    ),
+                                    CarouselColumn(
+                                        thumbnail_image_url='https://example.com/item2.jpg',
+                                        title='this is menu2',
+                                        text='description2',
+                                        actions=[
+                                            PostbackTemplateAction(
+                                                label='postback2',
+                                                text='postback text2',
+                                                data='action=buy&itemid=2'
+                                            ),
+                                            MessageTemplateAction(
+                                                label='message2',
+                                                text='message text2'
+                                            ),
+                                            URITemplateAction(
+                                                label='uri2',
+                                                uri='http://example.com/2'
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
     if 'hi' in text or 'hello' in text :
         profile = line_bot_api.get_profile(event.source.user_id)
         #if isinstance(event.source, SourceUser):
@@ -78,6 +126,8 @@ def handle_text_message(event):
         line_bot_api.push_message(event.source.user_id, TextSendMessage(text = "hey !!! how are you people"))
     elif text == 'template':
         line_bot_api.reply_message(event.reply_token, templatemessage)
+    elif text == 'carousel':
+        line_bot_api.reply_message(event.reply_token, message_carousel)
     else :
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "I am not being trained to understand you"))
         
