74d328a0c7e6deb46f03489ccd927a36a13b108f
Rohit Roy <64767006+blackbriar07@users.noreply.github.com>
2021-06-29 22:54:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -253,9 +253,9 @@ def handle_text_message(event):
         line_bot_api.push_message(event.source.user_id, TextSendMessage(text = "hey !!! how are you people"))
     elif 'chat' in text:
         #line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "oh yes"))
-        line_bot_api.reply_message(event.reply_token, ImageCarouselmessage1)
+        line_bot_api.reply_message(event.reply_token, ImageCarouselmessage)
     elif 'surely' in text:
-        line_bot_api.reply_message(event.reply_token, ImageCarouselmessage1)
+        line_bot_api.reply_message(event.reply_token, ImageCarouselmessage)
     elif text == 'carousel':
         line_bot_api.reply_message(event.reply_token, message_carousel)
     elif 'confirm' in text:
