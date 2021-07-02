6fcd0aca384b76877fa8a6b52a58369c80abe536
Rohit Roy <64767006+blackbriar07@users.noreply.github.com>
2021-07-02 18:12:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -571,7 +571,7 @@ def handle_text_message(event):
     #line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text, quick_reply=quick_reply))
     if 'hi' in text or 'hello' in text :
         line_bot_api.reply_message(event.reply_token, confirmmessage)
-    elif 'feeling' in text:
+    elif 'feeling' in text and 'great' in text:
         line_bot_api.reply_message(event.reply_token, confirmmessage_great)
         #line_bot_api.reply_message(event.reply_token, bmessage)
     elif 'right' in text:
